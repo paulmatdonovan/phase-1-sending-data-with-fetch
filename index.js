58d2b529e5abb6486c727df2085e5cd7c964fa2c
Paul Donovan <paul.m.donovan@outlook.com>
2024-03-31 07:40:28 +1100

    addsSolution

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject);
+
+function submitData(name, email) {
+    const postData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", postData)
+        .then(function (response) {
+            return response.json();
+        })
+
+
+        .then(function (object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function (message) {
+            document.body.innerHTML = message;
+            // console.log(error.message);
+        });
+}
+submitData()
